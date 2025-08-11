e7acde8902624e56783b55152fc7916d4d3a90c8
steveybaby <steve@sharpers.com>
2025-08-11 13:38:18 -0700

    fix ical links to correct url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
   // The site property should be your final deployed URL
-  site: 'https://fll-astro.sharpers.com',
+  site: 'https://fll.sharpers.com',
   // Base path for GitHub Pages deployment
   // base: '/fll-astro',
   integrations: [mdx()],
